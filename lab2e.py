066451fad7a7878bf6422fb8a6b2b4fb721ca4f5
Jude Owusu <jowusu9@myseneca.ca>
2025-01-22 15:06:04 -0500

    lab2e

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#Author: Jude Owusu
+#AuthorID:103399176
+#Date Created: 01/22/25
+
+count = 10
+while count != 0:
+    print(count)
+    count = count - 1
+print('blast off!')
